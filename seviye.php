c31145de2dc3d3506b4ef35d4e080d2d212ac177
Serkan-Armtl <serkanarmtl0019@gmail.com>
2024-03-10 16:20:28 +0300

    Seviye - Düzeltme
    Düzeltme...

--- seviye.php
+++ seviye.php
@@ -2,7 +2,6 @@
 include 'ayar.php';
 include 'ukas.php';
 include 'fonksiyon.php';
-<?php
 $data = $db -> prepare("SELECT count(y_yorum) as yorumsayisi FROM yorumlar WHERE y_uye_id=2");
 $data -> execute();
 $_data = $data -> fetch(PDO::FETCH_ASSOC);
