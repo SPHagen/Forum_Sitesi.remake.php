32852185118f1a0febac98f3ecd53b756832b7a7
Serkan-Armtl <serkanarmtl0019@gmail.com>
2024-03-10 18:01:23 +0300

    Şikayet - Güncelleme
    Gerekli kısımlar eklendi.

--- sikayet.php
+++ sikayet.php
@@ -2,5 +2,10 @@
 include 'ayar.php';
 include 'ukas.php';
 include 'fonksiyon.php';
-echo 'Selam';
 ?>
+<center>
+<h2> LÜTFEN ŞİKAYETİNİZİ GİRİNİZ </h2>
+<textarea name = "mesaj" cols = "30" rows = "10"> </textarea>
+<br><br>
+<input type="submit">
+</center>
