0b5c80786143b04e31228d57fa2beef19aa314b3
Serkan-Armtl <serkanarmtl0019@gmail.com>
2024-03-28 13:44:54 +0300

    sikayet.php <> Güncelleme
    Yorumu şikayet etme sayfası oluşturuldu.

--- sikayet.php
+++ sikayet.php
@@ -1,11 +1,30 @@
+<center>
 <?php
+session_start();
 include 'ayar.php';
 include 'ukas.php';
 include 'fonksiyon.php';
+
+$UyeID = $_SESSION["uye_id"];
+$YorumID = $_GET['yid'];
+
+$data = $db -> prepare("SELECT * FROM konular");
+$data -> execute();
+$_data = $data -> fetch(PDO::FETCH_ASSOC);
+
+// Yorum Satırı
+
+echo "$UyeID, numaralı üyesiniz! $YorumID, numaralı yorumu şikayet ediyorsunuz!<br><br>";
+
+$VeriListeleme = $db -> prepare("SELECT * FROM yorumlar WHERE y_id=?");
+$VeriListeleme -> execute([
+  $_GET['yid']
+  ]);
+$VeriListeleme = $VeriListeleme -> fetchALL(PDO::FETCH_ASSOC);
+
+foreach($VeriListeleme as $Yin) {
+  echo $Yin["y_yorum"];
+  echo '<br><br>Şikayet Etmek İstediğiniz Yorumun Konusu: '.$Yin[''];
+}
 ?>
-<center>
-<h2> LÜTFEN ŞİKAYETİNİZİ GİRİNİZ </h2>
-<textarea name = "mesaj" cols = "30" rows = "10"> </textarea>
-<br><br>
-<input type="submit">
 </center>
