d9e82f18b69cfd58c5d8a3428816705fc39183a1
Serkan-Armtl <serkanarmtl0019@gmail.com>
2024-03-28 13:45:58 +0300

    ust_bilgi.php <> Güncelleme
    Üst bilgi kısmı düzenlendi.

--- ust_bilgi.php
+++ ust_bilgi.php
@@ -1,13 +1,11 @@
-<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
-
 <?php
 if (@$_SESSION['uye_id'])
 {
-    echo '<a href = "profil.php?kadi='.@$_SESSION["uye_kadi"].'">Profilime Git</a>
-    <a href = "uyelik.php?p=cikis"> Çıkış Yap </a>';
+    echo '<a href = "profil.php?kadi='.@$_SESSION["uye_kadi"].'"><font face="Cascadia Code" >Profilime Git</font></a>
+    <a href = "uyelik.php?p=cikis">Oturumu Sonlandır</a>';
 }
 else
 {
-    echo '<a href = "uyelik.php?p=kayit"> Üye Ol </a> veya <a href = "uyelik.php"> Giriş Yap </a>';
+    echo '<a href = "uyelik.php?p=kayit" class="KayitOl"><font face="Cascadia Code" >KAYIT OL</font></a><br><br>Veya<br><br><a href = "uyelik.php">GİRİŞ YAP</a>';
 }
 ?>
