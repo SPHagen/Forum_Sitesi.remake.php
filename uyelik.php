3e07bc3c82eb36a236b4508fc5f282d0ba93caa2
Serkan-Armtl <serkanarmtl0019@gmail.com>
2024-06-25 06:25:14 +0300

    Üyelik - Güncelleme
    Tasarım ve hata düzeltmeleri yapıldı.

--- uyelik.php
+++ uyelik.php
@@ -61,23 +61,40 @@
                     header("LOCATION:index.php");
                 }else{
                     ukas_kayit("<p class='text-warning'>Lütfen boş bırakmayınız!</p>", "<p class='text-danger'>Böyle bir eposta mevcut! Lütfen başka bir tane deneyiniz!</p>", "<p class='text-warning'>Böyle bir kullanıcı adı mevcut! Lütfen başka bir tane deneyiniz!</p>", "<p class='text-success'>Başarıyla kaydoldun! :)</p>", "index.php", "<p class='text-danger'>Kullanıcı adı veya şifre hatalı!</p>", "<p class='text-danger'>Kayıt başarısız!</p>", "<p>Şifreniz bir birine eşleşmiyor!</p>", "<p>Lütfen gerçek bir eposta giriniz!</p>");
-                    echo '<h1 class="text-center"><strong>Şimdi Kayıt Ol!</strong></h1>
+                    echo '<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>..:: Şimdi Kayıt Ol! ::..</title>
+                        <link rel = "StyleSheet" Href = "Sinfonia_Per_Hagen.css" style="text/css" />
+                    </head>
+                    <body>
+                    <div id = "Yapıcı" >
+                    <div id = "Üst" >
+                    <center><a href = "index.php" ><img src = "Kaplan_Pencesi.jpg" ></img></a></center>
+                    <Kategoriler><strong>Kayıt Ol</strong></Kategoriler>
+                    </div>
+                    <div id = "Merkez" >
+                    <center>
                     <form action="" method="POST">
-                        <strong>Ad Soyad:</strong><br>
-                        <input type="text" class="form-control" name="adsoyad"><br>
-                        <strong>Kullanıcı adı:</strong><br>
-                        <input type="text" class="form-control" name="kadi"><br>
-                        <strong>Şifre:</strong><br>
-                        <input type="password" class="form-control" name="sifre"><br>
-                        <strong>Şifre (Tekrar):</strong><br>
-                        <input type="password" class="form-control" name="sifret"><br>
-                        <strong>E-Posta:</strong><br>
-                        <input type="text" class="form-control" name="eposta"><br />
-                        <input type="submit" class="btn btn-block btn-dark" name="kayit" value="Kayıt Ol">
-                    </form>
-                    <hr>
-                    <a href="uyelik.php?p=giris" class="btn btn-block btn-secondary">Şimdi giriş yap!</a><br />
-                    <a href="index.php" class="text-dark"><small>&larr; Anasayfaya dön</small></a>';
+                    <br><br><strong>Ad Soyad:</strong>
+                    <input type="text" class="form-control" name="adsoyad"><br><br>
+                    <strong>Kullanıcı adı:</strong>
+                    <input type="text" class="form-control" name="kadi"><br><br>
+                    <strong>Şifre:</strong>
+                    <input type="password" class="form-control" name="sifre"><br><br>
+                    <strong>Şifre (Tekrar):</strong>
+                    <input type="password" class="form-control" name="sifret"><br><br>
+                    <strong>E-Posta:</strong>
+                    <input type="text" class="form-control" name="eposta"><br /><br>
+                    <input type= "submit" id = "Buton_giris_yap" name="kayit" value="Kayıt Ol">
+                    <br><br><strong><a href = "uyelik.php" ><h3>Giriş Yap!</h3></a></strong>
+                    </center>
+                    </div>
+                    </div>
+                    </body>
+                    </html>';
                 }
                 break;
 
@@ -86,20 +103,36 @@
                     header("LOCATION:index.php");
                 }else{
                     ukas_giris("index.php", "<p class='text-warning'>Lütfen boş bırakmayınız!</p>", "<p class='text-danger'>Kullanıcı adı veya şifre hatalı!</p>");
-
-                    echo '<h1 class="text-center"><strong>Giriş Yap</strong></h1>
+                    echo '<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>..:: Giriş Yap ::..</title>
+                        <link Rel = "Stylesheet" Style = "text/css" Href = "Sinfonia_Per_Hagen.css" />
+                    </head>
+                    <body>
+                    <div id = "Yapıcı" >
+                    <div id = "Üst" >
+                    <center><a href = "index.php" ><img src = "Kaplan_Pencesi.jpg" ></img></a></center>
+                    <Kategoriler><strong>Giriş Yap</strong></Kategoriler>
+                    </div>
+                    <div id = "Merkez" >
+                    <center>
                     <br /><form action="" method="POST">
-                        <strong>Kullanıcı Adı:</strong>
-                        <input type="text" class="form-control" name="kadi">
-                        <strong>Şifre:</strong>
+                        <br><br><strong>Kullanıcı Adı: </strong>
+                        <input type="text" class="form-control" name="kadi"><br><br>
+                        <hr><br><strong>Şifre:</strong>
                         <input type="password" class="form-control" name="sifre"><br />
-                        <input type="submit" class="btn btn-block btn-dark" name="giris" value="Giriş Yap">
+                        <br><br><input type="submit" name="giris" id = "Buton_giris_yap" value="Giriş Yap">
+                        <br><br><strong><a href = "uyelik.php?p=kayit" ><h3>Şimdi Kayıt Ol!</h3></a></strong>
                     </form>
-                    <a href="uyelik.php?p=kayit" class="btn btn-block btn-secondary mt-3">Şimdi kayıt ol!</a>
-                    <hr>
-                    <a href="index.php" class="text-dark"><small>&larr; Anasayfaya dön</small></a>';
+                    </center>
+                    </div>
+                    </div>
+                    </body>
+                    </html>';
                     }
                 break;
         }
-
-      ?>
+        ?>
